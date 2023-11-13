a69f43aebbda5730d5c3b2a9feb052b7f1ecb10b
Styrale <styrale.draw@gmail.com>
2023-11-13 12:57:57 +0100

    bugfix + deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "https://styrale.github.io/Projet8/",
+  base: "https://styrale.github.io",
   plugins: [react()],
 })
